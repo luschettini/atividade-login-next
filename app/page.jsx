d3576a4bb464ff40b50b0a91cabae595f47e8d65
luiza <luizaschettinii@gmail.com>
2025-03-21 14:41:33 -0300

    feat: create project

---
+++ app/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import "./loading.css"; 
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      router.push("/signin");
+    }, 2000);
+    return () => clearTimeout(timeout);
+  }, [router]);
+
+  return (
+    <div className="loading-container">
+      <div className="spinner"></div>
+      <p className="loading-text">Carregando...</p>
+    </div>
+  );
+}
